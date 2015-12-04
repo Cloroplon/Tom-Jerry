b87440e496df1d9cd7795d4307ffc3b341c9c042
Simooo93 <simooovanov@gmail.com>
2015-12-04 11:22:33 +0200

    Fix added for content.loader

---
+++ TomAndJerry/States/GameState.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TomAndJerry.States
+{
+    class GameState : State
+    {
+        private Texture2D texture2D;
+        
+
+        public override void LoadContent(ContentManager contentManager)
+        {
+            base.LoadContent(contentManager);
+           
+            texture2D = contentManager.Load<Texture2D>("Space");
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            // just a random pic to see if our game is working for now. You can remove it any time.
+            spriteBatch.Draw(texture2D,new Vector2(0,0));
+            
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            // TODO
+        }
+    }
+}
